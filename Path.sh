5acb044a99b1f4f164bb247516dfcc2042b71978
Julia Ebeling <juliaebeling@dyn-9-165.icecube.wisc.edu>
2018-04-04 09:34:10 -0500

    Version to check if Kelner multiplicities are reproduced

--- Path.sh
+++ Path.sh
@@ -1,2 +1,2 @@
-declare -x PYTHONPATH="/rest/B/CRPropaTest/lib/python2.7/site-packages/crpropa:/rest/B/CRPropaTest/python/"
-declare -x LD_LIBRARY_PATH="/rest/B/CRPropaTest/lib/"
+declare -x PYTHONPATH="/Users/juliaebeling/CRPropaTest/lib/python2.7/site-packages/crpropa:/Users/juliaebeling/CRPropaTest/python/"
+declare -x LD_LIBRARY_PATH="/Users/juliaebeling/CRPropaTest/lib/"
